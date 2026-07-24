5234cff9751e25fbf7c385f429e7d458bcb52c3c
chapaiprakriti <prakriti9745906712@gmail.com>
2026-07-24 20:51:11 +0545

    connected to mongodbatlas

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,5 @@ import { connectToMongoDB } from "./database/mongodb";
 connectToMongoDB();
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server local: http://localhost:${PORT}`);
-  console.log(`Server mobile: http://192.168.101.9:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
