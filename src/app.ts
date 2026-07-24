06663e3afba8b8928cf70f142b106e3594ddfd9b
chapaiprakriti <prakriti9745906712@gmail.com>
2026-07-24 21:36:39 +0545

    managed app.ts

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,11 @@ const app: Application = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 app.use(express.static(path.join(process.cwd(), "public")));
